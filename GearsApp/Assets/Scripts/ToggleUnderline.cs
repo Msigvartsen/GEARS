90510e83c443696a91d0c94c49fceec10e4b327c
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-01-24 15:34:03 +0100

    Working on underlines and toggle buttons

--- GearsApp/Assets/Scripts/ToggleUnderline.cs
+++ GearsApp/Assets/Scripts/ToggleUnderline.cs
@@ -6,16 +6,45 @@ using UnityEngine.UI;
 public class ToggleUnderline : MonoBehaviour
 {
     public GameObject underline;
-    private bool isActive = false;
+
+    private Toggle toggle;
+    private Color selectedColor;
+    private Color normalColor;
+    private ColorBlock cb;
 
     private void Start()
     {
-        underline.SetActive(isActive);
-    }
+        toggle = GetComponent<Toggle>();
+        underline.SetActive(toggle.isOn);
 
+        //cb = toggle.colors;
+        //selectedColor = new Color(252, 154, 59, 255);
+        //normalColor = new Color(208, 92, 3, 255);
+        //UpdateColors(toggle.isOn);
+    }
+    
     public void ToggleActive()
     {
-        isActive = !isActive;
-        underline.SetActive(isActive);
+        underline.SetActive(toggle.isOn);
+        //UpdateColors(toggle.isOn); 
+    }
+
+    private void UpdateColors(bool isActive)
+    {
+
+    //    if(isActive)
+    //    {
+    //        cb.normalColor = selectedColor;
+    //        cb.highlightedColor = selectedColor;
+    //        cb.pressedColor = selectedColor;
+    //    }
+    //    else
+    //    {
+    //        cb.normalColor = normalColor;
+    //        cb.highlightedColor = normalColor;
+    //        cb.pressedColor = normalColor;
+    //    }
+
+    //    toggle.colors = cb;
     }
 }
