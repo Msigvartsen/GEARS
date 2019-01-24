3b458e46e8433f20bc846e15f7390f164ee9047c
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-01-24 21:33:49 +0100

    Fixed Toggle Colors

--- GearsApp/Assets/Scripts/ToggleUnderline.cs
+++ GearsApp/Assets/Scripts/ToggleUnderline.cs
@@ -8,43 +8,38 @@ public class ToggleUnderline : MonoBehaviour
     public GameObject underline;
 
     private Toggle toggle;
-    private Color selectedColor;
-    private Color normalColor;
-    private ColorBlock cb;
+    public Color selectedColor;
+    public Color normalColor;
+    private Image img;
+
 
     private void Start()
     {
         toggle = GetComponent<Toggle>();
         underline.SetActive(toggle.isOn);
-
-        //cb = toggle.colors;
-        //selectedColor = new Color(252, 154, 59, 255);
-        //normalColor = new Color(208, 92, 3, 255);
-        //UpdateColors(toggle.isOn);
+        img = GetComponent<Image>();
+        selectedColor = new Color(0.9882354f, 0.6039216f, 0.2313726f, 1);
+        normalColor = new Color(0.8156863f, 0.3607843f, 0.01176471f, 1);
+        UpdateColors(toggle.isOn);
     }
-    
+
     public void ToggleActive()
     {
         underline.SetActive(toggle.isOn);
-        //UpdateColors(toggle.isOn); 
+        img.color = selectedColor;
+        UpdateColors(toggle.isOn); 
     }
 
     private void UpdateColors(bool isActive)
     {
 
-    //    if(isActive)
-    //    {
-    //        cb.normalColor = selectedColor;
-    //        cb.highlightedColor = selectedColor;
-    //        cb.pressedColor = selectedColor;
-    //    }
-    //    else
-    //    {
-    //        cb.normalColor = normalColor;
-    //        cb.highlightedColor = normalColor;
-    //        cb.pressedColor = normalColor;
-    //    }
-
-    //    toggle.colors = cb;
+        if (isActive)
+        {
+            img.color = selectedColor;
+        }
+        else
+        {
+            img.color = normalColor;
+        }
     }
 }
