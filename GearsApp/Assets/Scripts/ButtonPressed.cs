8f3bc28d547815bf01eea1bec05a2984b0cca3aa
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-01-24 13:43:51 +0100

    Working with toggle buttons

---
+++ GearsApp/Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPressed : MonoBehaviour
+{
+    public bool isActive = false;
+    public Button button;
+
+    private void Start()
+    {
+        //button.onClick.AddListener(toggleActive);
+        this.enabled = isActive;
+       
+    }
+
+    private void Update()
+    {
+        //this.enabled = isActive;
+    }
+
+    private void toggleActive()
+    {
+        isActive = !isActive;
+        Debug.Log("Flipping button");
+
+    }
+}
---
+++ GearsApp/Assets/Scripts/ToggleUnderline.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleUnderline : MonoBehaviour
+{
+    public GameObject underline;
+    private bool isActive = false;
+
+    private void Start()
+    {
+        underline.SetActive(isActive);
+    }
+
+    public void ToggleActive()
+    {
+        isActive = !isActive;
+        underline.SetActive(isActive);
+    }
+}
