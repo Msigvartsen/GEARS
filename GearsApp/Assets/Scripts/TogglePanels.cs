d6dd2c2e9994c8bd709d34b76683aa64eb85a1ed
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-01-30 11:32:10 +0100

    Added Object tab in UI (3D Models Tab)

---
+++ GearsApp/Assets/Scripts/TogglePanels.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TogglePanels : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] panels;
+
+    public void ToggleUIPanel()
+    {
+        foreach(GameObject panel in panels)
+        {
+            if(panel.activeSelf)
+            {
+                panel.SetActive(false);
+            }
+            else
+            {
+                panel.SetActive(true);
+            }
+        }
+    }
+}
