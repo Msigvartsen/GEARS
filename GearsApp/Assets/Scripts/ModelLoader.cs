4b876f6d584bad17b2f63e99db2d0dc62bc61e43
Henning Krogh <henning.krogh@hotmail.com>
2019-01-30 20:10:39 +0100

    Cant get 3d models panel to work properly, no models appear.

--- GearsApp/Assets/Scripts/ModelLoader.cs
+++ GearsApp/Assets/Scripts/ModelLoader.cs
@@ -11,6 +11,7 @@ public class ModelLoader : MonoBehaviour
 
     private GameObject[] modelsInList;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,7 @@ public class ModelLoader : MonoBehaviour
         modelsInList[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
         modelsInList[1] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         modelsInList[2] = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-
+        print(modelsInList.Length);
     }
 
     // Update is called once per frame
@@ -30,19 +31,32 @@ public class ModelLoader : MonoBehaviour
 
     public void LoadCube()
     {
-        modelToSee.GetComponent<MeshFilter>().mesh = modelsInList[0].GetComponent<MeshFilter>().mesh;
+        //Mesh mesh = modelsInList[0].GetComponent<MeshFilter>().sharedMesh;
+        //Mesh mesh2 = Instantiate(mesh);
+        //modelToSee.GetComponent<MeshFilter>().sharedMesh = mesh2;
+
+        //Instantiate(modelsInList[0]);
+        //modelToSee = modelsInList[0];
+        //modelToSee.GetComponent<MeshFilter>().mesh = modelsInList[0].GetComponent<MeshFilter>().mesh;
+
+        modelToSee = modelsInList[0];
+
         modelToSee.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+        print(modelToSee.transform.position);
     }
 
     public void LoadSphere()
     {
-        modelToSee.GetComponent<MeshFilter>().mesh = modelsInList[1].GetComponent<MeshFilter>().mesh;
+        modelToSee = modelsInList[1];
+        //modelToSee.GetComponent<MeshFilter>().mesh = modelsInList[1].GetComponent<MeshFilter>().mesh;
         modelToSee.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
     }
 
     public void LoadCylinder()
     {
-        modelToSee.GetComponent<MeshFilter>().mesh = modelsInList[2].GetComponent<MeshFilter>().mesh;
+        modelToSee = modelsInList[2];
+        //modelToSee.GetComponent<MeshFilter>().mesh = modelsInList[2].GetComponent<MeshFilter>().mesh;
         modelToSee.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
     }
 }
