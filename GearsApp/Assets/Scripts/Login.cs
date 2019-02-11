c92f593ddcd154db3d11eea4cb057b77622e8b17
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-02-11 15:34:49 +0100

    Working on PHP server side script for registration and Login

---
+++ GearsApp/Assets/Scripts/Login.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Login : MonoBehaviour
+{
+    [Header("Input Fields")]
+    public InputField usernameField;
+    public InputField passwordField;
+
+    [Header("Buttons")]
+    public Button loginButton;
+
+    public void CallLogin()
+    {
+        StartCoroutine(UserLogin());
+    }
+
+    IEnumerator UserLogin()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("user", usernameField.text);
+        form.AddField("password", passwordField.text);
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post("http://localhost/gears/login.php", form))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log("Login Succeeded");
+                Debug.Log(webRequest.downloadHandler.text);
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
