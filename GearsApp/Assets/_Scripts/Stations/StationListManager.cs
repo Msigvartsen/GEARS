34988a706752c3757d41c91db2cdeb5e39fa550d
Henning Krogh <henning.krogh@hotmail.com>
2019-03-14 10:46:45 +0100

    Fixed station button error

--- GearsApp/Assets/_Scripts/Stations/StationListManager.cs
+++ GearsApp/Assets/_Scripts/Stations/StationListManager.cs
@@ -37,27 +37,29 @@ public class StationListManager : MonoBehaviour
 
     GameObject GetListItem(int index)
     {
-        print(index);
         GameObject go = Instantiate(Resources.Load<GameObject>("_Prefabs/" + prefabName));
         go.transform.SetParent(parent.transform, false);
         go.GetComponent<StationListItem>().station = stationArray[index];
-        Toggle visitedToggle = GetComponentInChildren<Toggle>();
-        visitedToggle.isOn = false; // stationArray[index].visited;
+        Toggle visitedToggle = go.GetComponentInChildren<Toggle>();
+        visitedToggle.isOn = stationArray[index].visited;
+
+        print(stationArray[index].visited);
 
         if (!visitedToggle.GetComponent<Toggle>().isOn)
         {
+            print("Station " + stationArray[index].station_NR + " in location " + stationArray[index].location_ID + " is turned off");
             visitedToggle.GetComponent<Image>().enabled = false;
-            GetComponentInChildren<CanvasGroup>().alpha = 0.5f;
-            GetComponentInChildren<Button>().enabled = false;
+            go.GetComponentInChildren<CanvasGroup>().alpha = 0.5f;
+            go.GetComponentInChildren<Button>().enabled = false;
             visitedToggle.GetComponentInChildren<Text>().text = "Locked";
             visitedToggle.GetComponentInChildren<Text>().transform.localPosition = new Vector3(0,0,0);
         }
         else
         {
-            print(prefabName + index + " is turned on");
+            print("Station " + stationArray[index].station_NR + " in location " + stationArray[index].location_ID + " is turned on");
             visitedToggle.GetComponent<Image>().enabled = true;
-            GetComponentInChildren<CanvasGroup>().alpha = 1;
-            GetComponentInChildren<Button>().enabled = true;
+            go.GetComponentInChildren<CanvasGroup>().alpha = 1;
+            go.GetComponentInChildren<Button>().enabled = true;
             visitedToggle.GetComponentInChildren<Text>().text = "Unlocked";
             visitedToggle.GetComponentInChildren<Text>().transform.localPosition = new Vector3(0, -100, 0);
         }
