1c0398c05c5160f3b7cddc21bc893fa16f14cccd
Henning Krogh <henning.krogh@hotmail.com>
2019-03-21 16:39:59 +0100

    User can interact with models at stations

--- GearsApp/Assets/_Scripts/Stations/StationModelInteraction.cs
+++ GearsApp/Assets/_Scripts/Stations/StationModelInteraction.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StationModelInteraction : MonoBehaviour
 {
@@ -8,14 +9,18 @@ public class StationModelInteraction : MonoBehaviour
     private Camera currentCamera;
 
     private GameObject selectedObject;
-    private int selectModelThreshold = 250;
-    private float moveSpeed = 1f;
+    private int selectModelThreshold = 25;
+    private float moveSpeed = 0.1f;
+
+    Vector2 beginTouchPos = new Vector2(0, 0);
+    Vector2 endTouchPos = new Vector2(0, 0);
+    float startTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentCamera = Camera.current;
-        
+        currentCamera = Camera.main;
+
         if (transform.childCount > 0)
         {
             model = transform.GetChild(0).gameObject;
@@ -67,36 +72,57 @@ public class StationModelInteraction : MonoBehaviour
     void DetermineTouchCountOneAction(Touch inTouch)
     {
         // Determine how much the users finger has moved
-        Vector2 beginTouchPos = new Vector2(0, 0);
-        Vector2 endTouchPos = new Vector2(0, 0);
 
         if (inTouch.phase == TouchPhase.Began)
         {
             beginTouchPos = inTouch.position;
+            startTime = Time.time;
         }
 
         if (inTouch.phase == TouchPhase.Moved)
         {
             if (selectedObject != null)
             {
+                // Get previous touch positions
                 Vector2 touchPrevPos = inTouch.position - inTouch.deltaPosition;
-                float prevTouchDeltaMagnitude = (touchPrevPos).magnitude;
-                float touchDeltaMagnitude = (inTouch.position).magnitude;
+                Vector2 touchPrevPosRight = new Vector2(inTouch.position.x - inTouch.deltaPosition.x, 0);
+                Vector2 touchPrevPosUp = new Vector2(0, inTouch.position.y - inTouch.deltaPosition.y);
 
-                float deltaMagnitudeDifference = (prevTouchDeltaMagnitude - touchDeltaMagnitude) * moveSpeed;
+                // Get magnitudes for previous touch
+                float prevTouchRightDeltaMagnitude = touchPrevPosRight.magnitude;
+                float prevTouchUpDeltaMagnitude = touchPrevPosUp.magnitude;
 
+                // Get current touch positions
+                Vector2 currentPos = inTouch.position;
+                Vector2 currentPosRight = new Vector2 (inTouch.position.x , 0);
+                Vector2 currentPosUp = new Vector2(0, inTouch.position.y);
+
+                // Get magnitudes for current touch
+                float touchRightDeltaMagnitude = currentPosRight.magnitude;
+                float touchUpDeltaMagnitude = currentPosUp.magnitude;
+
+                // Get magnitude differences between previous and current touch
+                float deltaRightMagnitudeDifference = (prevTouchRightDeltaMagnitude - touchRightDeltaMagnitude) * moveSpeed;
+                float deltaUpMagnitudeDifference = (prevTouchUpDeltaMagnitude - touchUpDeltaMagnitude) * moveSpeed;
+
+                // Transform the position of selected object based on magnitude differences
                 Vector3 right = currentCamera.transform.right;
-                selectedObject.transform.localPosition += right * deltaMagnitudeDifference * Time.deltaTime;
+                Vector3 up = currentCamera.transform.forward;
+                selectedObject.transform.localPosition -= right * deltaRightMagnitudeDifference * Time.deltaTime;
+                selectedObject.transform.localPosition -= up * deltaUpMagnitudeDifference * Time.deltaTime;
             }
+
         }
 
         if (inTouch.phase == TouchPhase.Ended)
         {
+            // Check how far the user has moved their finger since the beginning of the touchphase
             endTouchPos = inTouch.position;
             float distance = (endTouchPos - beginTouchPos).magnitude;
 
-            // Try to select a model if the users finger has moved relatively little
-            if (distance < selectModelThreshold)
+            // Try to select a model if the users finger has moved relatively 
+            // little and make sure the user is not holding down
+            if (distance < selectModelThreshold && (Time.time - startTime) < 1)
             {
                 SelectModel(inTouch);
             }
