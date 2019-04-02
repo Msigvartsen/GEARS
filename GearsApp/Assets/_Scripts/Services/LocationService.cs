2b921bb17271130e63e94bff0820e8766a525d82
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-04-02 14:32:17 +0200

    Minor changes to LocationService

--- GearsApp/Assets/_Scripts/Services/LocationService.cs
+++ GearsApp/Assets/_Scripts/Services/LocationService.cs
@@ -1,55 +1,66 @@
-﻿using System.Collections;
+﻿using Mapbox.Utils;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 #if PLATFORM_ANDROID
 using UnityEngine.Android;
 #endif
-
-public static class LocationService
+namespace LocationServiceNS
 {
-    public static void CallUserPermission()
+
+
+    public static class LocationService
     {
+        public static void CallUserPermission()
+        {
 #if PLATFORM_ANDROID
-        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
-        {
-            Permission.RequestUserPermission(Permission.FineLocation);
-        }
+            if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+            {
+                Permission.RequestUserPermission(Permission.FineLocation);
+            }
 #endif
-    }
-
-    public static IEnumerator StartLocationService()
-    {
-        if (!Input.location.isEnabledByUser)
-        {
-            yield break;
-        }
-        Input.location.Start();
-
-        // Wait until service initializes
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-        {
-            yield return new WaitForSeconds(1);
-            maxWait--;
         }
 
-        // Service didn't initialize in 20 seconds
-        if (maxWait < 1)
+        public static IEnumerator StartLocationService()
         {
-            Debug.Log("Connection to LocationService Timed Out");
-            yield break;
+            if (!Input.location.isEnabledByUser)
+            {
+                yield break;
+            }
+            Input.location.Start();
+
+            // Wait until service initializes
+            int maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);
+                maxWait--;
+            }
+
+            // Service didn't initialize in 20 seconds
+            if (maxWait < 1)
+            {
+                Debug.Log("Connection to LocationService Timed Out");
+                yield break;
+            }
+
+            if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                Debug.Log("Unable to determine device location");
+                yield break;
+            }
         }
 
-        if (Input.location.status == LocationServiceStatus.Failed)
+        public static void StopLocationService()
         {
-            Debug.Log("Unable to determine device location");
-            yield break;
+            Input.location.Stop();
         }
-    }
 
-    public static void StopLocationService()
-    {
-        Input.location.Stop();
+        public static Vector2d GetLatitudeLongitude()
+        {
+            Vector2d latlong = new Vector2d(Input.location.lastData.latitude, Input.location.lastData.longitude);
+            return latlong;
+        }
     }
 }
