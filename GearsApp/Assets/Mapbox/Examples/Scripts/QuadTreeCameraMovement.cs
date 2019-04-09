1324006494708f9cd39775f80fe8ed112733ccd8
Henning Krogh <henning.krogh@hotmail.com>
2019-04-09 18:37:14 +0200

    Zoom change

--- GearsApp/Assets/Mapbox/Examples/Scripts/QuadTreeCameraMovement.cs
+++ GearsApp/Assets/Mapbox/Examples/Scripts/QuadTreeCameraMovement.cs
@@ -133,7 +133,7 @@
 
 		void ZoomMapUsingTouchOrMouse(float zoomFactor)
 		{
-			var zoom = Mathf.Max(4.0f, Mathf.Min(_mapManager.Zoom + zoomFactor * _zoomSpeed, 17.0f));
+			var zoom = Mathf.Max(3.0f, Mathf.Min(_mapManager.Zoom + zoomFactor * _zoomSpeed, 17.0f));
 			if (Math.Abs(zoom - _mapManager.Zoom) > 0.0f)
 			{
 				_mapManager.UpdateMap(_mapManager.CenterLatitudeLongitude, zoom);
