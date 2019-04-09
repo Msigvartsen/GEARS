81468fe22f5d80135381a733437b1de84ac4433b
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-04-09 10:38:16 +0200

    Updates locations to UI (Need to change prefab)

--- GearsApp/Assets/_Scripts/Location/LocationListManager.cs
+++ GearsApp/Assets/_Scripts/Location/LocationListManager.cs
@@ -4,36 +4,42 @@ using UnityEngine.UI;
 
 public class LocationListManager : MonoBehaviour
 {
-    public enum ArrayType{ Locations, Favorites};
+    public enum ArrayType { Locations, Favorites };
     public ArrayType arrayType;
     private GameObject parent;
     private GameObject[] itemList;
     private Location[] locationArray;
     private string prefabName;
-
-    private void Start()
-    {
-        Init();
-        UpdateLocationList();
-    }
+    private bool isInitiated = false;
 
     private void Init()
     {
         parent = transform.gameObject;
         locationArray = LocationController.GetInstance().locationList.ToArray();
         prefabName = "ListItem";
+        isInitiated = true;
+    }
+
+
+    public void AddLocationList()
+    {
+        if (!isInitiated)
+        {
+            Init();
+            UpdateLocationList();
+        }
     }
 
     private void UpdateLocationList()
     {
         Location[] locations = new Location[0];
-    
-        if(arrayType == ArrayType.Favorites)
+
+        if (arrayType == ArrayType.Favorites)
         {
             List<Location> list = new List<Location>();
             for (int i = 0; i < locationArray.Length; i++)
             {
-                if(locationArray[i].favorite == true)
+                if (locationArray[i].favorite == true)
                 {
                     Debug.Log("Adding to Favlist: " + locationArray[i].name);
                     list.Add(locationArray[i]);
@@ -59,7 +65,7 @@ public class LocationListManager : MonoBehaviour
         GameObject go = Instantiate(Resources.Load<GameObject>("_Prefabs/" + prefabName));
         go.transform.SetParent(parent.transform, false);
         go.GetComponent<LocationListItem>().location = locations[index];
-        if(arrayType == ArrayType.Favorites)
+        if (arrayType == ArrayType.Favorites)
         {
             Debug.Log("Favorite List Item index: " + index + locations[index].name);
         }
