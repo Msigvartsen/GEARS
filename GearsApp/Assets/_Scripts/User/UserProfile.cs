17449635c9acdadc3fd53ff4a680043fc152bb06
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-04-12 12:35:25 +0200

    Username, level and experience updates in main scene

--- GearsApp/Assets/_Scripts/User/UserProfile.cs
+++ GearsApp/Assets/_Scripts/User/UserProfile.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 
 public class UserProfile : MonoBehaviour
 {
-    public Transform loadingBar;
     private User _currentUser;
 
+    [SerializeField]
+    private Transform loadingBar;
     [SerializeField]
     private TMPro.TextMeshProUGUI username;
     [SerializeField]
     private TMPro.TextMeshProUGUI level;
     [SerializeField]
     private TMPro.TextMeshProUGUI experience;
+
+
+    private void Start()
+    {
+        UpdateUserProfileUI();
+    }
 
     public void UpdateUserProfileUI()
     {
@@ -22,16 +29,51 @@ public class UserProfile : MonoBehaviour
         if (_currentUser == null)
             return;
 
-        username.text = _currentUser.username;
+        UpdateExperienceBar();
+        UpdateTextFields();
+        SetProfilePicture();
+    }
 
+    private void UpdateExperienceBar()
+    {
         int xp = _currentUser.experience;
         float xpFillAmount = (float)xp / 100;
         loadingBar.GetComponent<Image>().fillAmount = xpFillAmount;
+    }
 
-        if (experience != null && level != null)
+    private void UpdateTextFields()
+    {
+        if (username != null)
+            username.text = _currentUser.username;
+
+        if (experience != null)
         {
-            experience.text = xp.ToString();
+            experience.text = _currentUser.experience.ToString();
+        }
+        if (level != null)
+        {
             level.text = _currentUser.level.ToString();
+        }
+    }
+
+    private void SetProfilePicture()
+    {
+        GameObject profilePicture = GameObject.FindGameObjectWithTag("ProfilePicture");
+
+        MediaController mediaController = MediaController.GetInstance();
+        if (mediaController != null && profilePicture != null)
+        {
+            foreach (Media media in mediaController.mediaList)
+            {
+                if (media.media_ID == _currentUser.media_ID)
+                {
+                    profilePicture.GetComponent<RawImage>().texture = media.image;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("MediaController or profilePicture == null in UserProfile.cs - SetProfilePicture");
         }
     }
 }
