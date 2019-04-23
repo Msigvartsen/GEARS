5504b3169e2f412fa4056ff00fa77d649a85e8c1
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-04-23 10:24:35 +0200

    Model List now closes popup when pressed

--- GearsApp/Assets/_Scripts/Models/LoadModelButton.cs
+++ GearsApp/Assets/_Scripts/Models/LoadModelButton.cs
@@ -7,56 +7,63 @@ using Vuforia;
 public class LoadModelButton : MonoBehaviour
 {
     public Model model;
-    private Toggle button;
+    private Button button;
     private bool loaded = false;
     private GameObject modelToShow;
 
     void Start()
     {
         GetComponentInChildren<Text>().text = model.model_name;
-        button = GetComponent<Toggle>();
-        button.onValueChanged.AddListener(delegate { LoadModel(); });
-        GetComponent<Toggle>().group = gameObject.transform.parent.GetComponent<ToggleGroup>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(CloseProfilePictureWindow);
+        button.onClick.AddListener(LoadModel);
     }
 
     public void LoadModel()
     {
-        if (button.isOn)
+        if (modelToShow != null && modelToShow.name != model.model_name)
         {
-            // Create the prefab if it is not loaded
-            if (!loaded)
-            {
-                modelToShow = Instantiate(Resources.Load<GameObject>("_Prefabs/" + model.model_name));
+            Destroy(modelToShow.gameObject);
+            modelToShow = null;
+            loaded = false;
+        }
 
-                // Get the renderer component in either child or on current object and turn it off
-                if (modelToShow.GetComponentsInChildren<Renderer>(true).Length > 0)
-                {
-                    var rendererComponents = modelToShow.GetComponentsInChildren<Renderer>(true);
-                    foreach (var componenent in rendererComponents)
-                    {
-                        componenent.enabled = false;
-                    }
-                }
-                else if (modelToShow.GetComponent<Renderer>())
-                {
-                    modelToShow.GetComponent<Renderer>().enabled = false;
-                }
+        // Create the prefab if it is not loaded
+        if (!loaded)
+        {
+            modelToShow = Instantiate(Resources.Load<GameObject>("_Prefabs/" + model.model_name));
 
-                // Set the ground plane to be the models parent
-                modelToShow.transform.parent = GameObject.FindGameObjectWithTag("GroundPlane").transform;
-                print("LoadModelButton found groundplane");
-                modelToShow.transform.localPosition = new Vector3(0, 0, 0);
-                loaded = true;
-            }
-            else
+            // Get the renderer component in either child or on current object and turn it off
+            if (modelToShow.GetComponentsInChildren<Renderer>(true).Length > 0)
             {
-                // Activate the model if its selected and loaded
-                modelToShow.SetActive(true);
+                var rendererComponents = modelToShow.GetComponentsInChildren<Renderer>(true);
+                foreach (var componenent in rendererComponents)
+                {
+                    componenent.enabled = false;
+                }
             }
+            else if (modelToShow.GetComponent<Renderer>())
+            {
+                modelToShow.GetComponent<Renderer>().enabled = false;
+            }
+
+            // Set the ground plane to be the models parent
+            modelToShow.transform.parent = GameObject.FindGameObjectWithTag("GroundPlane").transform;
+            print("LoadModelButton found groundplane");
+            modelToShow.transform.localPosition = new Vector3(0, 0, 0);
+            loaded = true;
         }
         else
         {
-            modelToShow.SetActive(false);
+            // Activate the model if its selected and loaded
+            modelToShow.SetActive(true);
         }
     }
+    public void CloseProfilePictureWindow()
+    {
+        GameObject p = GameObject.FindGameObjectWithTag("NewProfilePictureWindow");
+        p.GetComponent<Animator>().Play("Fade-out");
+
+    }
 }
+
