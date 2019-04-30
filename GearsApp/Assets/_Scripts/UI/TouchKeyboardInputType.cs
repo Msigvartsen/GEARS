aeb46c985caf64a3e71e3bba0a241fd235c87d0d
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-04-30 15:05:30 +0200

    Changed keyboard types

---
+++ GearsApp/Assets/_Scripts/UI/TouchKeyboardInputType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchKeyboardInputType : MonoBehaviour
+{
+    private string inputText;
+
+    private void Start()
+    {
+        //TouchScreenKeyboard.hideInput = true;
+    }
+    public void SetKeyboardTypeToText()
+    {
+        TouchScreenKeyboard.Open(inputText, TouchScreenKeyboardType.NamePhonePad);
+    }
+    public void SetKeyboardTypeToNumber()
+    {
+        TouchScreenKeyboard.Open(inputText, TouchScreenKeyboardType.NumberPad);
+    }
+}
