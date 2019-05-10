cbbf0756a9de45b2b6a9c683b3f54515f685823f
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-05-10 10:33:37 +0200

    Added comments to LocationService

--- GearsApp/Assets/_Scripts/Services/LocationService.cs
+++ GearsApp/Assets/_Scripts/Services/LocationService.cs
@@ -7,9 +7,15 @@ using UnityEngine.Android;
 #endif
 namespace LocationServiceNS
 {
-
+    /// <summary>
+    /// Static class that handles Location Services.
+    /// </summary>
     public static class LocationService
     {
+        /// <summary>
+        /// Requests permission from the user to access geolocation via android.
+        /// This needs to be active for the geolocation to work.
+        /// </summary>
         public static void CallUserPermission()
         {
 #if PLATFORM_ANDROID
@@ -20,13 +26,18 @@ namespace LocationServiceNS
 #endif
         }
 
-        public static IEnumerator StartLocationService()
+        /// <summary>
+        /// Try to start location service. Connection retries for 20 seconds before ending.
+        /// Will try to match desired accuracy. If this fails it will use a default value. Location each updateDistanceInMeters.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerator StartLocationService(float desiredAccuracyInMeters = 1, float updateDistanceInMeters = 5)
         {
             if (!Input.location.isEnabledByUser)
             {
                 yield break;
             }
-            Input.location.Start();
+            Input.location.Start(desiredAccuracyInMeters, updateDistanceInMeters);
 
             // Wait until service initializes
             int maxWait = 20;
@@ -50,11 +61,18 @@ namespace LocationServiceNS
             }
         }
 
+        /// <summary>
+        /// Stop Location Service from Running.
+        /// </summary>
         public static void StopLocationService()
         {
             Input.location.Stop();
         }
 
+        /// <summary>
+        /// Get longitude and latitude from mobile location
+        /// </summary>
+        /// <returns>Vector2d containing latitude and longitude to the users mobile</returns>
         public static Vector2d GetLatitudeLongitude()
         {
             Vector2d latlong = new Vector2d(0, 0);
@@ -62,10 +80,13 @@ namespace LocationServiceNS
             {
                 latlong = new Vector2d(Input.location.lastData.latitude, Input.location.lastData.longitude);
             }
-
             return latlong;
         }
 
+        /// <summary>
+        /// Checks if the location service is enabled and running
+        /// </summary>
+        /// <returns>Returns true if location service is active</returns>
         public static bool IsLocationServiceRunning()
         {
             return (Input.location.status == LocationServiceStatus.Running && Input.location.isEnabledByUser);
