677f4a8594cd1b89aac9212d076b91f79b7c83fb
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-05-11 12:00:55 +0200

    Commenting FavoriteController

--- GearsApp/Assets/_Scripts/Location/FavoriteController.cs
+++ GearsApp/Assets/_Scripts/Location/FavoriteController.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using ConstantsNS;
 
+/// <summary>
+/// Class responsible for adding and removing favorites by sending requests to database.
+/// Script is used on LocationListItem prefab, on the Images child. 
+/// </summary>
 public class FavoriteController : MonoBehaviour
 {
     [SerializeField]
@@ -9,12 +13,18 @@ public class FavoriteController : MonoBehaviour
     private GameObject imagePanel; //For filled/unfilled favorite icon
     private bool isFavorite;
 
+    /// <summary>
+    /// Run at creation.
+    /// </summary>
     private void Start()
     {
         isFavorite = listItem.Location.favorite;
         imagePanel.SetActive(isFavorite);
     }
 
+    /// <summary>
+    /// Toggles the favorite location and sends a request to update the database.
+    /// </summary>
     public void CallAddToFavorite()
     {
         isFavorite = !isFavorite;
@@ -35,13 +45,14 @@ public class FavoriteController : MonoBehaviour
         StartCoroutine(WebRequestController.PostRequest<PHPStatusHandler>(path, form, UpdateFavorite));
     }
 
+    /// <summary>
+    /// Action method. This is ran during the Coroutine post using WebRequestController. 
+    /// </summary>
+    /// <param name="handler">Keeps track of the status from the web request.</param>
     private void UpdateFavorite(PHPStatusHandler handler)
     {
-        if (handler.statusCode == false)
-        {
-            Debug.Log(handler.text);
+        if (!WebRequestController.CheckResponse(handler))
             return;
-        }
 
         var loc = listItem.Location;
         loc.favorite = isFavorite;
