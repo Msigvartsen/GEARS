064f7278ecc6909bca3d49fc5e281734673407c5
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-05-11 16:55:52 +0200

    Comments to Media and MediaController

--- GearsApp/Assets/_Scripts/Media/Media.cs
+++ GearsApp/Assets/_Scripts/Media/Media.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Serializable class. Retreives data from JSON via Database/PHP.
+/// </summary>
 [System.Serializable]
 public class Media
 {
+    //Variables that keeps track of data from Database/PHP script (Comes in JSON format)
     public int media_ID;
     public string medianame;
     public string mediatype;
 
+    //Variables set in Unity/C#
     public Texture2D image=null;
 }
--- GearsApp/Assets/_Scripts/Media/MediaController.cs
+++ GearsApp/Assets/_Scripts/Media/MediaController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Singleton Class. Requests, and keeps track of all media from database.
+/// </summary>
 public class MediaController : MonoBehaviour
 {
     public List<Media> MediaList { get; set; }
     private static MediaController instance;
 
+    /// <summary>
+    /// Get instance of singleton.
+    /// </summary>
+    /// <returns>Instance of Singleton.</returns>
     public static MediaController GetInstance()
     {
         return instance;
     }
 
+    /// <summary>
+    /// Runs when app i starting. Creates singleton object.
+    /// Makes call to database.
+    /// </summary>
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -25,19 +36,23 @@ public class MediaController : MonoBehaviour
         CallRequestImages();
     }
 
+    /// <summary>
+    /// Start Coroutine with request to database.
+    /// </summary>
     public void CallRequestImages()
     {
         string path = ConstantsNS.Constants.PhpPath + "getimages.php";
         StartCoroutine(WebRequestController.GetRequest<WebResponse<Media>>(path, InitMediaList));
     }
 
+    /// <summary>
+    /// Action Method - Ran from Coroutine in WebRequestController.
+    /// </summary>
+    /// <param name="res">Response from Database</param>
     private void InitMediaList(WebResponse<Media> res)
     {
-        if(res.handler.statusCode == false)
-        {
-            Debug.Log(res.handler.text);
+        if (!WebRequestController.CheckResponse(res.handler))
             return;
-        }
 
         MediaList = new List<Media>();
         foreach (Media media in res.objectList)
