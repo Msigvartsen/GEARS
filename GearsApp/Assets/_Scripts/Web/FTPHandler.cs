05a52d69f699e6ecc7e9e57300412cfcc885acba
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-05-12 17:19:12 +0200

    Comments to FTPHandler

--- GearsApp/Assets/_Scripts/Web/FTPHandler.cs
+++ GearsApp/Assets/_Scripts/Web/FTPHandler.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using UnityEngine;
-using Newtonsoft.Json;
 
+/// <summary>
+/// Static class for downloading files from a FTP Server.
+/// </summary>
 public static class FTPHandler
 {
+    /// <summary>
+    /// Downloads one image from FTP server.
+    /// </summary>
+    /// <param name="serverUri">Uri path to image on server</param>
+    /// <returns>Returns downloaded image as Texture2D</returns>
     public static Texture2D DownloadImageFromFTP(Uri serverUri)
     {
         byte[] data = DownloadDataAsByte(serverUri);
-
+        //Create a new texture with size 2,2 (Size doesnt matter here, it will be overwritten by downloaded image).
         Texture2D file = new Texture2D(2, 2);
         if(data != null)
             file.LoadImage(data);
@@ -19,6 +25,11 @@ public static class FTPHandler
         return file;
     }
 
+    /// <summary>
+    /// Downloads all images in a folder from FTP Server.
+    /// </summary>
+    /// <param name="serverUri">Uri path to image on server</param>
+    /// <returns></returns>
     public static List<Texture2D> DownloadAllImagesFromFTP(Uri serverUri)
     {
         List<Texture2D> images = new List<Texture2D>();
@@ -30,10 +41,12 @@ public static class FTPHandler
         
         List<string> lines = new List<string>();
 
+        //Set up response and Stream.
         using (FtpWebResponse listResponse = (FtpWebResponse)listRequest.GetResponse())
         using (Stream listStream = listResponse.GetResponseStream())
         using (StreamReader listReader = new StreamReader(listStream))
         {
+            //Read all lines and insert into string list. If item starts with "." or ".." (paths) they will be skipped.
             while (!listReader.EndOfStream)
             {
                 string item = listReader.ReadLine();
@@ -44,11 +57,11 @@ public static class FTPHandler
                 }
             }
         }
-
+        //Loop through all string lines (containing paths to images) and download each image as Texture2D,
+        //and add them to a list.
         foreach(string line in lines)
         {
             Uri fileUri = new Uri(serverUri.ToString() + line);
-            Debug.Log("FTP FILE URLS : " + fileUri.ToString());
             Texture2D tex = DownloadImageFromFTP(fileUri);
             images.Add(tex);
         }
@@ -57,6 +70,11 @@ public static class FTPHandler
         
     }
 
+    /// <summary>
+    /// Download text file from FTP Server
+    /// </summary>
+    /// <param name="serverUri">Uri path to image on server</param>
+    /// <returns>Returns text file as string.</returns>
     public static string DownloadTextFromFTP(Uri serverUri)
     {
         byte[] data = DownloadDataAsByte(serverUri);
@@ -69,6 +87,11 @@ public static class FTPHandler
             return null;
     }
 
+    /// <summary>
+    /// Download data from FTP server as Bytes.
+    /// </summary>
+    /// <param name="serverUri">Uri path to image on server</param>
+    /// <returns>Returns all data in Byte format.</returns>
     private static byte[] DownloadDataAsByte(Uri serverUri)
     {
         WebClient request = ConnectToFTPClient(serverUri);
@@ -80,7 +103,13 @@ public static class FTPHandler
         else
             return null;
     }
-    
+
+    /// <summary>
+    /// Connection to the FTP Client server. A text asset (credentials.json) is needed containing correct
+    /// log in. (Json format needs to match Credentials class).
+    /// </summary>
+    /// <param name="serverUri">Uri path to image on server</param>
+    /// <returns>Returns request to client with status.</returns>
     private static WebClient ConnectToFTPClient(Uri serverUri)
     {
         if (serverUri.Scheme != Uri.UriSchemeFtp)
@@ -96,6 +125,9 @@ public static class FTPHandler
     }
 }
 
+/// <summary>
+/// Credential class is used to read credential file, and connect to FTP server.
+/// </summary>
 class Credentials
 {
     public string servername;
