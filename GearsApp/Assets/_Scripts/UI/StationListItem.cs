2e52bc949303a296571a714b7fde16b845229988
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2019-05-13 13:44:44 +0200

    Refactor stationlistitem - changes to UI

--- GearsApp/Assets/_Scripts/UI/StationListItem.cs
+++ GearsApp/Assets/_Scripts/UI/StationListItem.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class StationListItem : MonoBehaviour
@@ -9,11 +7,23 @@ public class StationListItem : MonoBehaviour
 
     private GameObject parent;
     private Button listButton;
+    [Header("Image Panels")]
+    [SerializeField]
+    private RawImage lockImageGameObject;
+    [SerializeField]
+    private RawImage thumbnail;
+
+    [Header("Textures")]
+    [SerializeField]
+    private Texture2D lockedImage;
+    [SerializeField]
+    private Texture2D unlockedImage;
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "Station " + Station.station_NR.ToString();
+        //thumbnail.texture = GetStationThumbnail();
         listButton = GetComponentInChildren<Button>();
         listButton.onClick.AddListener(OpenStationTab);
     }
@@ -23,5 +33,27 @@ public class StationListItem : MonoBehaviour
         StationController manager = StationController.GetInstance();
         manager.CurrentStation = Station;
         // LoadingScreen.LoadScene("Station");
+    }
+
+    public void UpdateStationStatus()
+    {
+        if (!Station.visited)
+        {
+            GetComponentInChildren<Button>().enabled = false;
+            GetComponentInChildren<CanvasGroup>().alpha = 0.5f;
+            GetComponentInChildren<Text>().text = "Locked";
+
+            if (lockedImage != null)
+                lockImageGameObject.texture = lockedImage;
+        }
+        else
+        {
+            GetComponentInChildren<Button>().enabled = true;
+            GetComponentInChildren<CanvasGroup>().alpha = 1f;
+            GetComponentInChildren<Text>().text = "Unlocked";
+
+            if (unlockedImage != null)
+                lockImageGameObject.texture = unlockedImage;
+        }
     }
 }
